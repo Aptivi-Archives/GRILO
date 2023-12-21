a108d49eea91c0224849e047170ec81dcf730614
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-12-21 10:06:58 +0300

    upd - Updated to .NET 8
    ---
    
    Type: upd
    Breaking: False
    Doc Required: False
    Part: 1/1

--- tools/pack.cmd
+++ tools/pack.cmd
@@ -6,12 +6,8 @@ if "%releaseconfig%" == "" set releaseconfig=Release
 
 :packbin
 echo Packing binary...
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-bin6.zip "..\GRILO.Bootloader\bin\%releaseconfig%\net8.0\*"
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-bin7.zip "..\GRILO.Bootloader\bin\%releaseconfig%\net7.0\*"
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-bin.zip "..\GRILO.Bootloader\bin\%releaseconfig%\net8.0\*"
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-bin48.zip "..\GRILO.Bootloader\bin\%releaseconfig%\net48\*"
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-demo6.zip "..\GRILO.BootableAppDemo\bin\%releaseconfig%\net8.0\*"
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-demo7.zip "..\GRILO.BootableAppDemo\bin\%releaseconfig%\net7.0\*"
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-demo.zip "..\GRILO.BootableAppDemo\bin\%releaseconfig%\net8.0\*"
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-demo48.zip "..\GRILO.BootableAppDemo\bin\%releaseconfig%\net48\*"
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-boot.zip "..\GRILO.Boot\bin\%releaseconfig%\netstandard2.0\*"
@@ -20,10 +16,6 @@ echo There was an error trying to pack binary (%errorlevel%).
 goto :finished
 
 :complete
-move %temp%\%version%-bin6.zip
-move %temp%\%version%-demo6.zip
-move %temp%\%version%-bin7.zip
-move %temp%\%version%-demo7.zip
 move %temp%\%version%-bin.zip
 move %temp%\%version%-demo.zip
 move %temp%\%version%-bin48.zip
